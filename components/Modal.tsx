cc7412b5dea7cd1dc0868ad997ff5c7c8b9024a8
kadir <develioglu306@gmail.com>
2022-11-21 03:16:36 +0300

    Fix vercel bug

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -89,7 +89,7 @@ function Modal() {
         <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
           <div className="space-y-6 text-lg">
             <div className="flex items-center space-x-2 text-sm">
-              <p className="font-semibold text-green-400">{movie?.vote_average * 10}% Match</p>
+              <p className="font-semibold text-green-400">{movie?.vote_average ? movie.vote_average * 10 : 0}% Match</p>
               <p className="font-light">{movie?.release_date || movie?.first_air_date}</p>
               <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
                 HD
@@ -99,7 +99,7 @@ function Modal() {
               <p className="w-5/6">{movie?.overview}</p>
               <div className="flex flex-col space-y-3 text-sm">
                 <div>
-                  <span className="text-[gray]">Genres:</span> {genres?.map((genre) => genre.name).join(', ')}
+                  <span className="text-[gray]">Genres:</span> {genres?.map((genre) => genre?.name).join(', ')}
                 </div>
 
                 <div>
